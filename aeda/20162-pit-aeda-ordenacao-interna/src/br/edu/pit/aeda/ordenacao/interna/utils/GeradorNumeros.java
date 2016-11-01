8f66e5b30b771acd269eab7060a9247da0ee2754
Waldir R. Pires Jr <wrpires@fitec.org.br>
2016-11-01 14:46:05 -0200

    Pre-setup for the random number generator. AEDA

--- aeda/20162-pit-aeda-ordenacao-interna/src/br/edu/pit/aeda/ordenacao/interna/utils/GeradorNumeros.java
+++ aeda/20162-pit-aeda-ordenacao-interna/src/br/edu/pit/aeda/ordenacao/interna/utils/GeradorNumeros.java
@@ -10,11 +10,31 @@
 // **********************************************************************
 package br.edu.pit.aeda.ordenacao.interna.utils;
 
+import java.io.File;
+import java.io.IOException;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
 import java.util.Random;
+
+import br.edu.pit.aeda.ordenacao.interna.io.EscritorDeArquivo;
 
 public class GeradorNumeros
 {
 
+  public static void gerarNumeros(String nomeArquivo, int tam, boolean gerarSeArquivoExistir) throws IOException, NoSuchAlgorithmException{
+    int a[] = GeradorNumeros.gerarNumeros(0, tam, tam, true);
+    if (!new File(nomeArquivo).exists() || (new File(nomeArquivo).exists() && gerarSeArquivoExistir)){
+      EscritorDeArquivo.escreverEmArquivo(a, "dados-"+tam);
+      System.out.println("Antes da ordenação: " + tam);
+      System.out.println(SegurancaUtil.gerarHashMd5(Arrays.toString(a)));
+      System.out.println();    
+    } else {
+      System.out.println("Arquivo nao gerado");
+    }
+  }
+  
+
+  
     public static int[] gerarNumeros(int min, int max, int quant, boolean positivo)
     {
         int[] a = new int[quant];
